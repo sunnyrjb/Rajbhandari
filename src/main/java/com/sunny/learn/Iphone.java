6079112db7a5a5e37bcb903a6d8927cf67841857
sunnyrjb <sunny@rjb@gmail.com>
2026-07-27 21:58:08 -0500

    2nd commit

---
+++ src/main/java/com/sunny/learn/Iphone.java
@@ -0,0 +1,11 @@
+package com.sunny.learn;
+
+public class Iphone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Iphone");
+
+	}
+
+}
